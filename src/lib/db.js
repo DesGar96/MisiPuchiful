0fd8e2b241a85f11e54c8853e10a0a567522d706
DesiGar96 <desireegarcia.dgaa@gmail.com>
2026-03-05 23:14:51 +0100

    version 05-03-26 despliegue

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,36 @@
 import mysql from 'mysql2/promise';
 
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL === 'true' ? {} : false,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0
+});
+
+export { pool };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Crear el pool de conexiones - usa el nombre exacto de la BD
-export const pool = mysql.createPool({
+/*export const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',  // Vacío si es XAMPP por defecto
@@ -22,4 +51,4 @@ export async function testConnection() {
     console.error('❌ Error de conexión:', error.message);
     return false;
   }
-}
+}*/
