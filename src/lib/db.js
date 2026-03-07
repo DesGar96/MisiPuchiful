96a5b9ac468270e2eb6ccfc0a13866ede1d8fdad
DesiGar96 <desireegarcia.dgaa@gmail.com>
2026-03-07 18:46:50 +0100

    version lib

--- src/lib/db.js
+++ src/lib/db.js
@@ -6,7 +6,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: process.env.DB_SSL === 'true' ? {} : false,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0
@@ -22,31 +24,3 @@ export { pool };
 
 
 
-
-
-
-
-
-// Crear el pool de conexiones - usa el nombre exacto de la BD
-/*export const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',  // Vacío si es XAMPP por defecto
-  database: 'misipuchiful',  // AHORA EN MINÚSCULAS
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-// Función para probar la conexión
-export async function testConnection() {
-  try {
-    const connection = await pool.getConnection();
-    console.log('✅ Conexión a MySQL exitosa');
-    connection.release();
-    return true;
-  } catch (error) {
-    console.error('❌ Error de conexión:', error.message);
-    return false;
-  }
-}*/
